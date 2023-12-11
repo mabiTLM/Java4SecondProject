a22c0d3ed6e0bb63cb87c421cdb6adb9607de9ea
mabiTLM <mabitlm@naver.com>
2023-12-11 18:02:56 +0900

    feat : 퍼블리케이션 작업중

--- projectSecond/src/main/java/com/nanoClone/projectSecond/publications/dao/PublicationsDAO.java
+++ projectSecond/src/main/java/com/nanoClone/projectSecond/publications/dao/PublicationsDAO.java
@@ -1,8 +1,22 @@
 package com.nanoClone.projectSecond.publications.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import com.nanoClone.projectSecond.newsBoard.domain.NewsBoard;
 
 @Repository
 public class PublicationsDAO {
-
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+  private RowMapper<NewsBoard> mapper = new RowMapper<NewsBoard>() {
+    @Override
+    public NewsBoard mapRow(ResultSet rs, int rowNum) throws SQLException {
+      return new NewsBoard(rs.getInt("id"), rs.getString("category"), rs.getString("title"),
+          rs.getString("content"), rs.getDate("created_at"), rs.getInt("views"));
+    }
+  };
 }
