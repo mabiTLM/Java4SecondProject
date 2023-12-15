4d3e4fdc5128af82bed5dd8ef07d00daeaccb3fb
mabiTLM <mabitlm@naver.com>
2023-12-15 18:02:07 +0900

    feat: 배포 설정까지 완료

--- projectSecond/src/main/java/com/nanoClone/projectSecond/ProjectSecondApplication.java
+++ projectSecond/src/main/java/com/nanoClone/projectSecond/ProjectSecondApplication.java
@@ -2,9 +2,15 @@ package com.nanoClone.projectSecond;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectSecondApplication {
+public class ProjectSecondApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ProjectSecondApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectSecondApplication.class, args);
