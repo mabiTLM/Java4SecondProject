00bf2330be5df30a5e282e47574c2035deabe1a2
mabiTLM <mabitlm@naver.com>
2023-12-16 09:47:34 +0900

    fix:경로수정

--- projectSecond/src/main/java/com/nanoClone/projectSecond/journals/controller/JournalsController.java
+++ projectSecond/src/main/java/com/nanoClone/projectSecond/journals/controller/JournalsController.java
@@ -83,8 +83,11 @@ public class JournalsController {
     String[] tempImageNames = originImageName.split("[.]");
     String extImage = originImageName.substring(originImageName.indexOf("."));
     String randomImageName = UUID.randomUUID() + extImage;
-    String saveImagePath = System.getProperty("user.dir")
-        + "\\src\\main\\resources\\static\\images\\journals\\upload\\" + randomImageName;
+    String saveImagePath =
+        "/home/tomcat/apache-tomcat-10.1.17/webapps/ROOT/WEB-INF/classes/static/images/journals/upload/"
+            + randomImageName;
+    // String saveImagePath = System.getProperty("user.dir")
+    // + "\\src\\main\\resources\\static\\images\\journals\\upload\\" + randomImageName;
     String uploadImageUrl = "/images/journals/upload/" + randomImageName;
     File saveImageFile = new File(saveImagePath);
     try {
